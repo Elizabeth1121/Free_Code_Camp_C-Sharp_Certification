3f2674b36f01b5c3192933b36557215a8286d337
Elizabeth <itsybitsy1121@outlook.com>
2024-07-08 12:53:10 -0400

    cleaned up the code in program.cs and added a new cs file in Modify_The_Content_Of_Strings project

--- Modify_The_Content_Of_Strings/Modify_The_Content_Of_Strings/obj/Debug/net8.0/Modify_The_Content_Of_Strings.AssemblyInfo.cs
+++ Modify_The_Content_Of_Strings/Modify_The_Content_Of_Strings/obj/Debug/net8.0/Modify_The_Content_Of_Strings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modify_The_Content_Of_Strings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+024ee2187e959aea213e7aae9eed9cf3282916fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4484f2a1eb236b217bcd62b51269f8f3697b19f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modify_The_Content_Of_Strings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modify_The_Content_Of_Strings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
