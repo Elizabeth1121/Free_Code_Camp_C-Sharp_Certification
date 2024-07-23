bf51069885d97e1f3d86dfc93dfee0f780583d3d
Elizabeth <itsybitsy1121@outlook.com>
2024-07-23 12:27:26 -0400

    created a using methods in C# project

---
+++ Methods_In_C-Sharp/Methods_In_C-Sharp/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Before calling method");
+SayHello();
+Console.WriteLine("After calling method");
+
+void SayHello()
+{
+    Console.WriteLine("Hello World");
+}
+
+int[] a = {1, 2, 3, 4, 5};
+
+Console.WriteLine("Contents of Array:");
+PrintArray();
+
+void PrintArray()
+{
+    foreach(var number in a)
+    {
+        Console.WriteLine($"{number}");
+    }
+    Console.WriteLine();
+}
