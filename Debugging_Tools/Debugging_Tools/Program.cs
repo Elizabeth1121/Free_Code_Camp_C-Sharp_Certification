f6296d892b01ca49e2067fa2813e255ff2db48b0
Elizabeth <itsybitsy1121@outlook.com>
2024-11-07 14:27:22 -0800

    completed the Debug tools project

--- Debugging_Tools/Debugging_Tools/Program.cs
+++ Debugging_Tools/Debugging_Tools/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  
+This code instantiates a value and then calls the ChangeValue method
+to update the value. The code then prints the updated value to the console.
+*/
+int x = 5;
+
+x = ChangeValue(x);
+
+Console.WriteLine(x);
+
+int ChangeValue(int value) 
+{
+    return value = 10;
+}
+
+// SOLUTION: the value was never updated becasue we never assigned the return value of the ChangeValue method to the variable x, and the method was set to void so it didn't return anything.
--- Debugging_Tools/Debugging_Tools/obj/Debug/net8.0/Debugging_Tools.AssemblyInfo.cs
+++ Debugging_Tools/Debugging_Tools/obj/Debug/net8.0/Debugging_Tools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Debugging_Tools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b4305531a9fec41f838932ee7986f7699a93f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a3b31c59b103ab02ba9ca493db7cdec073e5222")]
 [assembly: System.Reflection.AssemblyProductAttribute("Debugging_Tools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Debugging_Tools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
